75b35709073790661b6038df0ae6bdec2c58eb1a
Ahmad Ramadhan <ahmadramadhan13@gmail.com>
2023-07-26 05:38:47 +0700

    add chromium-min

--- src/lib/crawl.js
+++ src/lib/crawl.js
@@ -1,6 +1,6 @@
 // import puppeteer from 'puppeteer-core';
 import puppeteerExtra from 'puppeteer-extra'
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 
 import 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
 import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
@@ -26,10 +26,11 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 export const Crawl = async (url, email, pass, reUrl) => {
   const browser = await puppeteerExtra.use(StealthPlugin()).launch({ 
     headless: chromium.headless,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v111.0.0-pack.tar'),
     // for dev
     // executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe", /* chrome://version/ */
-    args: [...chromium.args, "--disable-notifications"] 
+    args: [...chromium.args, "--disable-notifications"],
+    ignoreHTTPSErrors: true
   });
   const page = await browser.newPage();
 
